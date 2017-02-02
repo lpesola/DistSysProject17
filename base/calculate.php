a49d9cf252ffeb454af02cad733437543ae8fd3c
L Pesola <lpesola@cs.helsinki.fi>
2017-02-02 13:54:14 +0200

    basic version done (simple form)

---
+++ base/calculate.php
@@ -0,0 +1,40 @@
+<?php 
+
+session_start();
+$_SESSION["history"] = array();
+
+$arg1 = filter_input(INPUT_GET, "arg1", FILTER_VALIDATE_INT);
+$arg2 = filter_input(INPUT_GET, "arg2", FILTER_VALIDATE_INT);
+$op = $_GET['op'];
+
+
+$result = "NaN";
+
+switch ($op) {
+	case "+":
+		$result = $arg1 + $arg2;
+	case "-":
+		$result = $arg1 - $arg2;       
+       		break;
+	case "*":
+		$result = $arg1 * $arg2;
+        	break;
+	case "/":
+		$result = $arg1 / $arg2;
+		break;
+    	default:
+        	exit("invalid operator");
+}
+
+$calculation = array($arg1, $op, $arg2, $result);
+array_push($_SESSION["history"], $calculation);
+
+echo "<div style=\"background-color:grey;color:white;padding:20px;\">";
+foreach($_SESSION["history"] as &$calc) {
+	echo "$calc[0] $calc[1] $calc[2] = $calc[3] <br />";
+}
+echo "</div>";
+
+include("calculator.html");
+
+?>
